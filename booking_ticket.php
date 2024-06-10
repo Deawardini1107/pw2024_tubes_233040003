68f5a8e37e87b62df80d101bf13bdf356bd115b4
Your Name <you@example.com>
2024-06-10 22:03:58 +0700

    upade bug

--- booking_ticket.php
+++ booking_ticket.php
@@ -26,7 +26,7 @@ if (!$booking) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Home One || Tour & Travel HTML Template</title>
+    <title>Time Traveler</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Place favicon.ico in the root directory -->
